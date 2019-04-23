7289cdfe28e34b068b58dc770f319a97d4b7e3af
Eyarotskiy <eyarotskiy94@gmail.com>
2019-04-23 17:45:54 +0200

    Changing file structure

--- polyfills.js
+++ polyfills.js
@@ -5784,8 +5784,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Yevhen\Programming\Projects\angular-new-app\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! D:\Yevhen\Programming\Projects\angular-new-app\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! D:\Yevhen\Programming\Projects\Vova\PureGolf\puregolf_web\PureGolfUI\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\Yevhen\Programming\Projects\Vova\PureGolf\puregolf_web\PureGolfUI\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
